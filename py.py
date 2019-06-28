26d2997f8a4ed83c26fcb19d2ea6e8a3c5f4232d
bustedgans <50026266+bustedgans@users.noreply.github.com>
2019-06-28 17:10:31 +0700

    Add files via upload

---
+++ py.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import time
+
+print('')
+
+print ('+[-------Coded By Insan Ganz-------]+')
+time.sleep(1)
+print ('+[Tools Yang Dibuat Ketika Gw Gabut Jancok]+')
+time.sleep(1)
+print ('+[Contact Me : 081285405915]+')
+time.sleep(1)
+print ('+[-------Team : FRI3NDS CYBER ARMY-------]+')
+
+menu = '''
+1. Cara Deketin Cewe Biar Lengket
+2. Cara Langgeng Hubungan Sama Pacar
+3. Apakah Gua Cakep?
+'''
+time.sleep(1)
+
+print (menu)
+jembut = input('Silahkan Pilih Tod : ')
+
+if jembut == '1':
+    print('1.Lu Harus Kenalan Dulu Lah Goblok')
+    time.sleep(1)
+    print('2.Kalo Udah Kenalan Lu Ajak Main Dulu Kemana Kek Lu Ajak Nonton,Dinner:v')
+    time.sleep(1)
+    print('3.Lu Bikin Dia Ketawa Rata Rata Cewek Suka Sama Cowok Yang Humoris')
+    time.sleep(1)
+    print('4.Nah Cewek Itu Kalo Milih Cowok Dari Hati Men Bukan Dari Fisik Atau harta')
+    print('Ngga Kaya Elu Para Kontolers')
+    time.sleep(3)
+    print('5.Kalo Udah Lu Tembak Boy Lu Kasih Bunga Atau Boneka:)')
+    time.sleep(1)
+    print('6.Semoga Di Terima Ya:D Semangat Menjalani Hari-Hari Dengan Baik Syg')
+    time.sleep(1)
+    print('See You Next Time Syg Unchhh Kyaaaa:v')
+
+if jembut == '2':
+    print('1.Lu Ajak Main Dulu Kemana Sampai Senja Datang')
+    time.sleep(1)
+    print('2.Kalo Sudah Mau Senja Lu Ajak Ke Kosan Elu:D')
+    time.sleep(1)
+    print('3.Kalo Udah Lu Kasih Makan Minum Usahain Minum nya Yang beralkohol:v')
+    time.sleep(2)
+    print('4.Nonton Bareng di kosan terserah mau nonton apa sampe si cewe tertidur')
+    time.sleep(2)
+    print('5.Kalo Udah Tertidur Lu Buka Baju Nya Pelan Pelan Dan Rasakan Surga Dunia')
+    time.sleep(2)
+    print('6.Sekian Tutor Dari Gw Semoga Berhasil Syg')
+    time.sleep(1)
+    print('Btw Gw Ga Punya Pacar Gan(^_^)')
+    time.sleep(3)
+
+if jembut == '3':
+    print('Iya Lu Cakep')
+    time.sleep(2)
+    print('Tapi Boong')
+    print('wkwkwkwk')
+
+else:
+  print('')
